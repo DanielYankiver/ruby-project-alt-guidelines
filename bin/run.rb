88eafcd14c5c9527e222ca01e7efd20d6aece368
Sarah Thiery <sarahvthiery@gmail.com>
2020-12-03 11:35:45 -0500

    2nd commit

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,7 @@
 require_relative '../config/environment'
 
 puts "hello world"
+puts User.first
+will = User.first
+puts will.past_orders
+will.display_cart
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,5 +1,29 @@
 class User < ActiveRecord::Base
-   
     has_many :orders
+
+    def past_orders
+        orders.where(checked_out: true)
+    end
+
+    def current_cart
+        # Look into order time and maybe add a conditional 
+        orders.find_or_create_by(checked_out: false, order_time: Time.now)
+    end
+
+    # def check_out_current_cart 
+
+
+    # end 
+
+
+    # def display_cart 
+    #     binding.pry
+    #     self.current_cart.icecreamorders.each do |icecreamorder|
+    #         puts "Order number: #{icecreamorder.id}. Flavor: #{icecreamorder.icecream.flavor}"
+    #         binding.pry
+    #     end 
+    # end 
+
+   
 
 end
